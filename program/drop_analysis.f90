ab6610b6535d98fa0d59f182470ce4df47775ccf
nakano-gendai <g_nakano@fm.me.es.osaka-u.ac.jp>
2024-05-23 19:48:02 +0900

    0523

--- program/drop_analysis.f90
+++ program/drop_analysis.f90
@@ -16,15 +16,15 @@ module globals
     integer,parameter:: xmax = 255 !ｘ方向格子数（０から数える）
     integer,parameter:: ymax = 255 !ｙ方向格子数（０から数える）
     integer,parameter:: zmax = 255 !ｚ方向格子数（０から数える）
-    integer,parameter:: step_start = 5000
-    integer,parameter:: step_end = 100000
+    integer,parameter:: step_start = 19000
+    integer,parameter:: step_end = 20000
     integer,parameter:: step_bin = 1000
     integer,parameter:: step_num2 = (step_end - step_start) / step_bin + 1 
 
     !読み込みディレクトリ
-    character(*),parameter :: datadir_input = "/data/sht/nakanog/DNS_turbulence_256_IHT/case1/"
+    character(*),parameter :: datadir_input = "/data/sht/nakanog/DNS_turbulence_256_IHT/case4/"
     !出力ディレクトリ
-    character(*),parameter :: datadir_output = "/data/sht/nakanog/DNS_turbulence_256_IHT/case1/large/"
+    character(*),parameter :: datadir_output = "/data/sht/nakanog/DNS_turbulence_256_IHT/case4/large/"
 
     !粒子速度（整数）
     integer,parameter:: cx(15) = (/0, 1, 0,  0, -1,  0,  0,  1, -1,  1,  1, -1,  1, -1, -1/)
@@ -57,7 +57,7 @@ module globals
     !その他変数
     real(8) dummy
     real(8) time1, time2 !計算時間測定用
-    integer i, j, k, n, xi, yi, zi, alpha, beta
+    integer i, j, n, xi, yi, zi, alpha, beta
     character :: filename*200
     character :: filename2*200
     character :: filename3*200
@@ -96,7 +96,7 @@ contains
 
     !========================並列数・コミュニケータ分割・通信先設定========================
         !配列のallocate(xi:0とx_procs+1がのりしろ)(yi:0とymax+2がのりしろ)(zi:0とz_procs+1がのりしろ)
-        Nx = 16 !x方向の並列数（ただし，Nx/=comm_procs）
+        Nx = 32 !x方向の並列数（ただし，Nx/=comm_procs）
         Ny = comm_procs / (Nx) !z方向の並列数
         x_procs = (xmax+1) / Nx
         y_procs = (ymax+1) / Ny
@@ -155,24 +155,41 @@ contains
         phi_procs(:,:,:) = 0.0d0
     end subroutine ini
 
-    subroutine variable(grad_phi_procs,chemical_potencial,free_energy,grad_free_energy,lap_phi_procs)
+    subroutine ini_fft(chemical_potencial_hat,advection_phi_hat)
+        complex(kind(0d0)), allocatable :: chemical_potencial_hat(:,:,:), advection_phi_hat(:,:,:)
+        
+        call decomp_2d_init(xmax+1, ymax+1, zmax+1, Nx, Ny)
+        call decomp_2d_fft_init(PHYSICAL_IN_Z)
+        call decomp_2d_fft_get_size(sta, last, sized)
+
+        allocate(chemical_potencial_hat(sta(1)-1:last(1)-1, sta(2)-1:last(2)-1, sta(3)-1:last(3)-1))
+        allocate(advection_phi_hat(sta(1)-1:last(1)-1, sta(2)-1:last(2)-1, sta(3)-1:last(3)-1))
+
+        chemical_potencial_hat(:,:,:) = (0.0d0, 0.0d0)
+        advection_phi_hat(:,:,:) = (0.0d0, 0.0d0)
+    end subroutine ini_fft
+
+    subroutine variable(grad_phi_procs,chemical_potencial,free_energy,grad_free_energy,lap_phi_procs,advection_phi)
         real(8),allocatable :: grad_phi_procs(:,:,:,:)
         real(8),allocatable :: chemical_potencial(:,:,:)
         real(8),allocatable :: free_energy(:,:,:)
         real(8),allocatable :: grad_free_energy(:,:,:,:)
         real(8),allocatable :: lap_phi_procs(:,:,:)
+        real(8),allocatable :: advection_phi(:,:,:)
 
         allocate(grad_phi_procs(1:3,1:x_procs,1:y_procs,1:zmax+1))
         allocate(chemical_potencial(1:x_procs,1:y_procs,1:zmax+1))
         allocate(free_energy(0:x_procs+1,0:y_procs+1,0:zmax+2))
         allocate(grad_free_energy(1:3,1:x_procs,1:y_procs,1:zmax+1))
         allocate(lap_phi_procs(1:x_procs,1:y_procs,1:zmax+1))
+        allocate(advection_phi(1:x_procs,1:y_procs,1:zmax+1))
 
         grad_phi_procs(:,:,:,:) = 0.0d0
         chemical_potencial(:,:,:) = 0.0d0
         free_energy(:,:,:) = 0.0d0
         grad_free_energy(:,:,:,:) = 0.0d0
         lap_phi_procs(:,:,:) = 0.0d0
+        advection_phi(:,:,:) = 0.0d0
     end subroutine variable
 
     subroutine input(u1_procs,u2_procs,u3_procs,p_procs,phi_procs)
@@ -265,8 +282,6 @@ contains
                         lap_f(xi,yi,zi) = lap_f(xi,yi,zi) + fun(xi+cx(i),yi+cy(i),zi+cz(i))
                     enddo
                     lap_f(xi,yi,zi) = lap_f(xi,yi,zi) / (5.0d0*ds**2)
-                        enddo
-                    enddo
                 enddo
             enddo
         enddo
@@ -279,25 +294,43 @@ contains
         do zi=1,zmax+1
             do yi=1,y_procs
                 do xi=1,x_procs
-                    free_energy(xi,yi,zi) = phi_procs(xi,yi,zi)*T*log(phi(xi,yi,zi)/(1.0d0-b*phi(xi,yi,zi))) - a*phi(xi,yi,zi)*phi(xi,yi,zi)
+                    free_energy(xi,yi,zi) = phi_procs(xi,yi,zi)*T*log(phi_procs(xi,yi,zi)/(1.0d0-b*phi_procs(xi,yi,zi))) - a*phi_procs(xi,yi,zi)*phi_procs(xi,yi,zi)
                 enddo
             enddo
         enddo
     end subroutine free_energy_cal
 
-    subroutine chemical_potencial_cal(chemical_potencial,grad_free_energy,lap_phi_procs)
+    subroutine chemical_potencial_cal(chemical_potencial,phi_procs,lap_phi_procs)
         real(8),intent(inout):: chemical_potencial(1:x_procs,1:y_procs,1:zmax+1)
-        real(8),intent(in):: free_energy(0:x_procs+1,0:y_procs+1,0:zmax+2)
+        real(8),intent(in):: phi_procs(0:x_procs+1,0:y_procs+1,0:zmax+2)
         real(8),intent(in):: lap_phi_procs(1:x_procs,1:y_procs,1:zmax+1)
 
         do zi=1,zmax+1
             do yi=1,y_procs
                 do xi=1,x_procs
-                    chemical_potencial(xi,yi,zi) = grad_free_energy(xi,yi,zi) - kappag*lap_phi_procs(xi,yi,zi)
+                    chemical_potencial(xi,yi,zi) = ( T*log( phi_procs(xi,yi,zi)/(1.0d0-b*phi_procs(xi,yi,zi)) ) + T/(1.0d0-b*phi_procs(xi,yi,zi)) - 2.0d0*a*phi_procs(xi,yi,zi) ) - kappag*lap_phi_procs(xi,yi,zi)
                 enddo
             enddo
         enddo
     end subroutine chemical_potencial_cal
+
+    subroutine advection_cal(advection_phi,grad_phi_procs,u1_procs,u2_procs,u3_procs)
+        real(8),intent(inout):: advection_phi(1:x_procs,1:y_procs,1:zmax+1)
+        real(8),intent(in):: grad_phi_procs(1:3,1:x_procs,1:y_procs,1:zmax+1)
+        real(8),intent(in):: u1_procs(0:x_procs+1,0:y_procs+1,0:zmax+2)
+        real(8),intent(in):: u2_procs(0:x_procs+1,0:y_procs+1,0:zmax+2)
+        real(8),intent(in):: u3_procs(0:x_procs+1,0:y_procs+1,0:zmax+2)
+
+        do zi=1,zmax+1
+            do yi=1,y_procs
+                do xi=1,x_procs
+                    advection_phi(xi,yi,zi) = u1_procs(xi,yi,zi)*grad_phi_procs(1,xi,yi,zi) &
+                                            + u2_procs(xi,yi,zi)*grad_phi_procs(2,xi,yi,zi) &
+                                            + u3_procs(xi,yi,zi)*grad_phi_procs(3,xi,yi,zi)
+                enddo
+            enddo
+        enddo
+    end subroutine advection_cal
 
     subroutine At_cal(grad_phi_procs,At_ini)
         real(8),intent(in):: grad_phi_procs(1:3,1:x_procs,1:y_procs,1:zmax+1)
@@ -320,30 +353,138 @@ contains
             if(n == step_start) then
                 At_ini = At_sum
                 open(103,file="./At_case29.d")
-                write(103,*) dble(n), dble(n)/tau_l, dble(n)/tau_k, At_sum / At_ini
+                write(103,*) dble(n), At_sum / At_ini
                 close(103)
             else
                 open(103,file="./At_case29.d",action="write",position="append")
-                write(103,*) dble(n), dble(n)/tau_l, dble(n)/tau_k, At_sum / At_ini
+                write(103,*) dble(n), At_sum / At_ini
                 close(103)
             endif
         endif
     end subroutine At_cal
 
+    subroutine contribution(chemical_potencial,advection_phi,chemical_potencial_hat,advection_phi_hat,enesupe_phi,enesupe_phi_sum,enesupe_phi_result)
+        real(8),intent(inout):: chemical_potencial(1:x_procs,1:y_procs,1:zmax+1)
+        real(8),intent(inout):: advection_phi(1:x_procs,1:y_procs,1:zmax+1)
+
+        complex(kind(0d0)), intent(inout) :: chemical_potencial_hat(sta(1)-1:last(1)-1, sta(2)-1:last(2)-1, sta(3)-1:last(3)-1)
+        complex(kind(0d0)), intent(inout) :: advection_phi_hat(sta(1)-1:last(1)-1, sta(2)-1:last(2)-1, sta(3)-1:last(3)-1)
+
+        real(8),intent(inout):: enesupe_phi((xmax+1)/2 + 1), enesupe_phi_sum((xmax+1)/2 + 1), enesupe_phi_result((xmax+1)/2 + 1)
+
+        complex(kind(0d0)) comp_tmp
+        integer k1, k2, k3, k(1:3), k_index
+        real(8) k_abs
+
+        !フーリエ変換（実数→複素数）
+        call fft_r2c(chemical_potencial, chemical_potencial_hat)
+        call fft_r2c(advection_phi, advection_phi_hat)
+
+        enesupe_phi(:) = 0.0d0
+        enesupe_phi_sum(:) = 0.0d0
+        do k3=sta(3)-1,last(3)-1
+            k(3) =( k3 - judge(k3, zmax+1) )
+            do k2=sta(2)-1,last(2)-1
+                k(2) = ( k2 - judge(k2, ymax+1) )
+                do k1=sta(1)-1,last(1)-1
+                    k(1) = ( k1 - judge(k1, xmax+1) )
+
+                    k_abs = ( dble(k(1)**2 + k(2)**2 + k(3)**2) )**0.5d0
+                    k_index = int( k_abs ) + 1
+
+                    if(k3 == 0) then
+                        comp_tmp = advection_phi_hat(k1,k2,k3) * conjg(chemical_potencial_hat(k1,k2,k3))
+                        enesupe_phi(k_index) = enesupe_phi(k_index) - 4.0d0 * pi * k_abs**2.0d0 * REAL(comp_tmp)
+                    else 
+                        comp_tmp = advection_phi_hat(k1,k2,k3) * conjg(chemical_potencial_hat(k1,k2,k3))
+                        enesupe_phi(k_index) = enesupe_phi(k_index) - 4.0d0 * pi * k_abs**2.0d0 * REAL(comp_tmp) * 2.0d0
+                    endif
+                enddo
+            enddo
+        enddo
+
+        call MPI_Reduce(enesupe_phi(1), enesupe_phi_sum(1), (xmax+1)/2 + 1, MPI_REAL8, MPI_SUM, 0, MPI_COMM_WORLD, ierr)
+
+        if(comm_rank == 0) then
+            do i = 1, (xmax+1)/2 + 1
+                enesupe_phi_result(i) = enesupe_phi_result(i) + enesupe_phi_sum(i)
+            enddo
+        endif
+
+        if(n == step_end) then
+            if(comm_rank==0) then
+                open(37,file ="./enesupe_phi.d")
+                do i=1,(xmax+1)/2+1
+                    enesupe_phi_result(i) = enesupe_phi_result(i) / dble(step_num2)
+                    write(37,"(2es16.8)") (dble(i)-1.0d0), enesupe_phi_result(i)
+                enddo
+                close(37)
+            endif
+        endif
+    endsubroutine contribution
+
+    !フーリエ変換
+    subroutine fft_r2c(q, q_hat)
+        real(8), intent(inout)            :: q(0:x_procs-1, 0:y_procs-1, 0:zmax)
+        complex(kind(0d0)), intent(inout) :: q_hat(sta(1)-1:last(1)-1, sta(2)-1:last(2)-1, sta(3)-1:last(3)-1)
+        call decomp_2d_fft_3d(q, q_hat)
+        q_hat(:,:,:) = q_hat(:,:,:) / (dble(xmax+1)*dble(ymax+1)*dble(zmax+1))
+    end subroutine fft_r2c
+
+    !逆フーリエ変換
+    subroutine fft_c2r(q_hat, q)
+        real(8), intent(inout)            :: q(0:x_procs-1, 0:y_procs-1, 0:zmax)
+        complex(kind(0d0)), intent(inout) :: q_hat(sta(1)-1:last(1)-1, sta(2)-1:last(2)-1, sta(3)-1:last(3)-1)
+        call decomp_2d_fft_3d(q_hat, q)
+    end subroutine fft_c2r
+
+    !ディレクトリ作成
+    subroutine mk_dirs(outdir)
+        implicit none
+        character(len=*), intent(in) :: outdir
+        character(len=256) command
+        write(command, *) 'if [ ! -d ', trim(outdir), ' ]; then mkdir -p ', trim(outdir), '; fi'
+        call system(command)
+    end subroutine mk_dirs
+
+    function judge(i, N)
+        integer :: i, N, judge
+        if(i >= N / 2 + 1)then
+            judge = N
+        else
+            judge = 0
+        endif
+    end function
 end module globals
 
 program main
 use globals
+use decomp_2d
+use decomp_2d_fft
+use decomp_2d_io
+use glassman
     implicit none
     real(8),allocatable :: p_procs(:,:,:), u1_procs(:,:,:), u2_procs(:,:,:), u3_procs(:,:,:), phi_procs(:,:,:)
     real(8),allocatable :: grad_phi_procs(:,:,:,:)
     real(8),allocatable :: chemical_potencial(:,:,:)
     real(8),allocatable :: free_energy(:,:,:), grad_free_energy(:,:,:,:)
-    real(8),allocatable :: lap_phi_procs(xi,yi,zi)
+    real(8),allocatable :: lap_phi_procs(:,:,:)
+    real(8),allocatable :: advection_phi(:,:,:)
     real(8):: cr(1:3, 1:15)  !粒子速度（実数）
     real(8) At_ini
+
+    !波数空間の変数
+    complex(kind(0d0)), allocatable :: chemical_potencial_hat(:,:,:), advection_phi_hat(:,:,:)
+    real(8),allocatable :: enesupe_phi(:), enesupe_phi_sum(:), enesupe_phi_result(:)
+
     At_ini = 0.0d0
     cr(:,:) = 0.0d0
+    allocate(enesupe_phi((xmax+1)/2 + 1))
+    allocate(enesupe_phi_sum((xmax+1)/2 + 1))
+    allocate(enesupe_phi_result((xmax+1)/2 + 1))
+    enesupe_phi(:) = 0.0d0
+    enesupe_phi_sum(:) = 0.0d0
+    enesupe_phi_result(:) = 0.0d0
 
 !==================================MPI並列開始===============================================
     call MPI_Init(ierr)
@@ -353,15 +494,44 @@ use globals
     ! call mk_dirs(datadir_output)
     call par(cr)
     call ini(p_procs,u1_procs,u2_procs,u3_procs,phi_procs)
-    call variable(grad_phi_procs,chemical_potencial,free_energy,grad_free_energy,lap_phi_procs)
+    call ini_fft(chemical_potencial_hat,advection_phi_hat)
+    call variable(grad_phi_procs,chemical_potencial,free_energy,grad_free_energy,lap_phi_procs,advection_phi)
 
 DO n = step_start, step_end, step_bin
     call input(u1_procs,u2_procs,u3_procs,p_procs,phi_procs)
     call glue(phi_procs)
+    call glue(u1_procs)
+    call glue(u2_procs)
+    call glue(u3_procs)
+    !化学ポテンシャル
+    call lap_cal(lap_phi_procs,phi_procs)
+    call chemical_potencial_cal(chemical_potencial,phi_procs,lap_phi_procs)
+    !移流項
     call grad_cal(grad_phi_procs,phi_procs,cr)
-    call At_cal(grad_phi_procs,At_ini)
+    call advection_cal(advection_phi,grad_phi_procs,u1_procs,u2_procs,u3_procs)
+    !各スケールの界面への寄与
+    call contribution(chemical_potencial,advection_phi,chemical_potencial_hat,advection_phi_hat,enesupe_phi,enesupe_phi_sum,enesupe_phi_result)
 ENDDO
 
+    ! if(comm_rank==0) then
+    !     open(37,file ="./enesupe_phi.d")
+    !     do i=1,(xmax+1)/2+1
+    !         enesupe_phi_result(i) = enesupe_phi_result(i) / dble(step_num2)
+    !         write(37,"(2es16.8)") dble(i)-1.0d0, enesupe_phi_result(i)
+    !     enddo
+    !     close(37)
+    ! endif
+
+!液滴の変形率の計算
+! DO n = step_start, step_end, step_bin
+!     call input(u1_procs,u2_procs,u3_procs,p_procs,phi_procs)
+!     call glue(phi_procs)
+!     call grad_cal(grad_phi_procs,phi_procs,cr)
+!     call At_cal(grad_phi_procs,At_ini)
+! ENDDO
+
 !================MPI並列終わり=======================================
+    call decomp_2d_fft_finalize
+    call decomp_2d_finalize
     call MPI_Finalize(ierr)
 end program main
